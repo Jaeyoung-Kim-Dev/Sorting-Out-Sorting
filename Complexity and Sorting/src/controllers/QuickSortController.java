2f3e139ed5c21872edb266001b54e0428f9e0486
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2020-10-25 17:48:19 -0600

    Add quick sort

--- Complexity and Sorting/src/controllers/QuickSortController.java
+++ Complexity and Sorting/src/controllers/QuickSortController.java
@@ -2,26 +2,35 @@ package controllers;
 
 import java.util.*;
 
-public class QuickSortController extends Controller {	
-	private int swapCount = 0;
-	private int comparisonCount = 0;
-	private int left = 0;
-	private int right = 0;
-	
-    public void sort(Comparable[] items, Comparator comparator) {
-    	this.right = items.length;
-    	//quickSort(items, comparator, left, right); 
-    } 
-    
-    public int getSwapCount() {
-		return swapCount;
+import shapes.Polygon;
+
+public class QuickSortController extends Controller {
+
+	public void sort(Comparable[] items, Comparator comparator) {
+		quicksort(items, 0, items.length - 1, comparator);
 	}
 
-	/**
-	 * @return the comparisonCount
-	 */
-	public int getComparisonCount() {
-		return comparisonCount;
+	public void quicksort(Comparable[] items, int start, int end, Comparator comparator) {
+		if (start < end) {
+			int pivotIndex = partition(items, start, end, comparator);
+			quicksort(items, start, pivotIndex - 1, comparator);
+			quicksort(items, pivotIndex + 1, end, comparator);
+		}
 	}
-    
+
+	private int partition(Comparable[] items, int start, int end, Comparator comparator) {
+		Polygon pivot = (Polygon) items[end];
+		int i = start;
+		for (int j = start; j <= end - 1; j++) {
+			if (comparator.compare((Polygon) items[j], pivot) < 0) {
+				Polygon tmp = (Polygon) items[i];
+				items[i] = items[j];
+				items[j] = tmp;
+				i++;
+			}
+		}
+		items[end] = items[i];
+		items[i] = pivot;
+		return i;
+	}
 }
